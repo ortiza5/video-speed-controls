5aa252324c9aa007d8dddd21db1fe52ec5ba0fbf
ortiza5 <43008712+ortiza5@users.noreply.github.com>
2020-04-13 01:38:30 -0600

    redesigning popup
    - disable hotkey option
    - notification and fullscreen layering option
    - got only needed icons from fontawesome

--- popup.js
+++ popup.js
@@ -1,90 +1,76 @@
 // Reference:
 // https://stackoverflow.com/questions/20019958/chrome-extension-how-to-send-data-from-content-script-to-popup-html
 
-
 // Update the relevant fields with the new data.
-const setInfo = info => {
-    // document.getElementById('total').textContent = info.total;
-    // document.getElementById('inputs').textContent = info.inputs;
-    // document.getElementById('buttons').textContent = info.buttons;
-};
+function updateValues(info) {
+  document.getElementById("video-speed").textContent = info.speed;
+  document.getElementById("website-domain").value = info.domain;
+  // document.getElementById('buttons').textContent = info.buttons;
+}
 
 function updateSpinner(change) {
-    var speedObj = document.getElementById("speed");
-    var value = parseFloat(speedObj.value);
-    if (change == "down") {
-        if (value >= 0.25) {
-            value = value - 0.25;
-        } else {
-            value = 0;
-        }
+  var speedObj = document.getElementById("video-speed");
+  var value = parseFloat(speedObj.value);
+  if (change == "down") {
+    if (value >= 0.25) {
+      value = value - 0.25;
     } else {
-        if (value <= 15.75) {
-            value = value + 0.25;
-        } else {
-            value = 16;
-        }
+      value = 0;
     }
-    speedObj.value = value;
+  } else {
+    if (value <= 15.75) {
+      value = value + 0.25;
+    } else {
+      value = 16;
+    }
+  }
+  speedObj.value = value;
 }
 
 // Once the DOM is ready...
-window.addEventListener('DOMContentLoaded', () => {
-    // ...query for the active tab...
-    chrome.tabs.query({
-        active: true,
-        currentWindow: true
-    }, tabs => {
-        // ...and send a request for the DOM info...
-        chrome.tabs.sendMessage(
-            tabs[0].id,
-            { from: 'popup', subject: 'Info' },
-            setInfo);
+window.addEventListener("DOMContentLoaded", () => {
+  // ...query for the active tab...
+  let params = {
+    active: true,
+    currentWindow: true,
+  };
+  chrome.tabs.query(params, function (tabs) {
+    // ...and send a request for the DOM info...
+    chrome.tabs.sendMessage(tabs[0].id, { from: "popup", subject: "Info" }, updateValues);
+
+    // Button to Disable the Controls for a Website
+    var disableButton = document.getElementById("disable");
+    disableButton.addEventListener("click", function () {
+      chrome.tabs.sendMessage(tabs[0].id, { from: "popup", subject: "disable" });
     });
-});
+  });
 
-window.addEventListener('load', function load(event) {
-    var downButton = document.getElementById('down');
-    downButton.addEventListener('click', function () { updateSpinner('down'); });
-});
+  // Video Speed Down Button
+  var downButton = document.getElementById("down");
+  downButton.addEventListener("click", function () {
+    updateSpinner("down");
+  });
 
-window.addEventListener('load', function load(event) {
-    var upButton = document.getElementById('up');
-    upButton.addEventListener('click', function () { updateSpinner('up'); });
-});
+  // Video Speed Up Button
+  var upButton = document.getElementById("up");
+  upButton.addEventListener("click", function () {
+    updateSpinner("up");
+  });
 
-window.addEventListener('load', function load(event) {
-    var resetButton = document.getElementById('reset');
-    resetButton.addEventListener('click', function () {
-        var speedObj = document.getElementById("speed");
-        // TODO: Add pulling value from saved storage
-        speedObj.value = 1;
+  // Button to Reset Video Speed
+  var resetButton = document.getElementById("reset");
+  resetButton.addEventListener("click", function () {
+    var speedObj = document.getElementById("speed");
+    // TODO: Add pulling value from saved storage
+    speedObj.value = 1;
+  });
 
-    });
-});
-
-window.addEventListener('load', function load(event) {
-    var saveButton = document.getElementById('save');
-    saveButton.addEventListener('click', function () {
-        var speedObj = document.getElementById("speed");
-        var value = parseFloat(speedObj.value);
-        var domainObj = document.getElementById("domain");
-        chrome.tabs.sendMessage(
-            tabs[0].id,
-            {
-                from: 'popup',
-                subject: 'save',
-                domain: domainObj.innerHTML,
-                speed: value
-            });
-    });
-});
-
-window.addEventListener('load', function load(event) {
-    var disableButton = document.getElementById('disable');
-    disableButton.addEventListener('click', function () {
-        chrome.tabs.sendMessage(
-            tabs[0].id,
-            { from: 'popup', subject: 'disable' });
-    });
+  // Button to Save Video Speed for a Website
+  var saveButton = document.getElementById("save");
+  saveButton.addEventListener("click", function () {
+    var speedObj = document.getElementById("video-speed");
+    var value = parseFloat(speedObj.value);
+    var domainObj = document.getElementById("website-domain").innerHTML;
+    // TODO: Save to localStorage
+  });
 });
