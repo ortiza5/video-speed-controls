d3833ecea6898f1010b0f8794e195aa779e0a6bf
ortiza5 <43008712+ortiza5@users.noreply.github.com>
2020-04-27 19:44:56 -0600

    hotkeys adding and removing works

--- options.js
+++ options.js
@@ -1,16 +1,3 @@
-let displayKeys = `
-        <div class="hotkeys btn" id="slower-btn">
-            <span class="shortcut-text">
-            </span>
-            <button class="close" title="Delete shortcut">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-                    <path d="M13.5 6l-1.4-1.4-3.1 3-3.1-3L4.5 6l3.1 3.1-3 2.9 1.5 1.4L9 10.5l2.9 2.9 1.5-1.4-3-2.9"></path>
-                </svg>
-            </button>
-
-        </div>
-    `;
-
 var SETTINGS_FULL;
 var HOTKEY_CODES;
 var HOTKEY_DISABLES;
@@ -68,12 +55,22 @@ function enterNewHotkey(event) {
 
     // once no more keys are pressed, the final combo is recorded
     if (keysDown.size === 0) {
-      // TODO: Save to storage
       window.removeEventListener("keydown", keyPress);
       window.removeEventListener("keyup", keyRelease);
-      setHotkeyBtn(element.parentNode);
+      // setHotkeyBtn(element.parentNode);
+      updateHotkey(element, [...keysFinal]);
     }
   }
+}
+
+function updateHotkey(element, newVal) {
+  let parentSection = element.parentNode;
+  parentSection.removeChild(element);
+  let newSettings = SETTINGS_FULL;
+  newSettings.hotkeys.codes[parentSection.id] = newVal;
+  chrome.storage.local.set({ "extension-settings": newSettings }, function () {
+    setHotkeyBtn(parentSection);
+  });
 }
 
 function formateHotkeys(set1) {
@@ -89,7 +86,7 @@ function formateHotkeys(set1) {
     Delete: "Del",
   };
 
-  let keyString = [...set1].map((c) => c.slice(0, 1).toUpperCase() + c.slice(1)).join(" + ");
+  let keyString = [...set1].map((c) => c.slice(0, 1).toUpperCase() + c.slice(1).toLowerCase()).join(" + ");
   keyString = keyString.replace(
     /Control|Arrowup|Arrowright|Arrowdown|Arrowleft|\s\s|Pageup|Pagedown|Delete/g,
     function (match) {
@@ -104,8 +101,10 @@ function formateHotkeys(set1) {
 function setHotkeyBtn(element) {
   let el = document.createElement("div");
   // TODO: Check if hotkey combo in storage
+
   // if in storage print stored combo
-  if (false) {
+  if (HOTKEY_CODES[element.id].length !== 0) {
+    console.log(element.id);
     // delete previous btn
     let old = document.getElementById(element.id + "-hotkey");
     if (old !== null && element.parentNode) {
@@ -113,6 +112,23 @@ function setHotkeyBtn(element) {
     }
     el.setAttribute("id", element.id + "-hotkey");
     el.setAttribute("class", "btn hotkeys");
+    el.innerHTML = `
+      <span class="btn-text">
+            ${formateHotkeys(new Set(HOTKEY_CODES[element.id]))}
+      </span>
+      <button class="close" title="Delete shortcut">
+          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+              <path d="M13.5 6l-1.4-1.4-3.1 3-3.1-3L4.5 6l3.1 3.1-3 2.9 1.5 1.4L9 10.5l2.9 2.9 1.5-1.4-3-2.9"></path>
+          </svg>
+      </button>
+    `;
+    el.getElementsByClassName("close")[0].addEventListener(
+      "click",
+      (deleteHotkey = function () {
+        updateHotkey(el, []);
+      })
+    );
+    element.appendChild(el);
   } else {
     // delete any previous btn
     let old = document.getElementById(element.id + "-btn");
@@ -123,15 +139,22 @@ function setHotkeyBtn(element) {
     el.setAttribute("id", element.id + "-btn");
     el.setAttribute("class", "btn btn-hov");
     el.innerHTML = `
-            <span class="btn-text">
-                Click to type a new shortcut
-            </span>
-        `;
+        <span class="btn-text">
+            Click to type a new shortcut
+        </span>
+    `;
     // Add listner for new hotkey input
     el.addEventListener("click", enterNewHotkey);
     element.appendChild(el);
   }
 }
 
-// Once the DOM is ready...
-getSettings(window.addEventListener("DOMContentLoaded", populateFields));
+getSettings(function () {
+  // once the DOM is ready file in all settings info
+  let stateCheck = setInterval(() => {
+    if (document.readyState === "complete") {
+      clearInterval(stateCheck);
+      populateFields();
+    }
+  }, 100);
+});
